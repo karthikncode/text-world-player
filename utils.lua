bbba76a3f3e0e98d58644f92c929d1bf20a26e8d
Karthik <nkartr@gmail.com>
2015-04-19 17:48:33 -0400

    initial attempt at Q-learn

--- utils.lua
+++ utils.lua
@@ -3,3 +3,11 @@ function sleep(n)  -- seconds
   local t0 = clock()
   while clock() - t0 <= n do end
 end
+
+function split(s, pattern)
+	local parts = {}
+	for i in string.gmatch(s, pattern) do
+  	table.insert(parts, i)
+	end
+	return parts
+end
